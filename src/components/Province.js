994f4680df8860975b9e0259402c63ad13857087
shreyhere <116891326+shreyhere@users.noreply.github.com>
2023-01-13 13:22:42 +0530

    Update Province.js

--- src/components/Province.js
+++ src/components/Province.js
@@ -1,25 +1,27 @@
-import React,{useState} from 'react'
-import City from './City'
-function Province ({name, cities, index }) {
-    const [clicked, setClicked] = useState(false);
-    function handleClick(){
-        setClicked(!clicked)
-    }
+import React from "react";
+import City from "./City";
 
-    return (
-        <li>
-            <h3 id={'state'+(index +1)} onClick={handleClick}>{name}</h3>
-            {clicked ? (
-                <ul>
-                {cities.map(item=>(
-                    <City 
-                    name={item.name} 
-                    towns={item.towns} 
-                    index={cities.indexOf(item)} />
-                ))}
-                </ul>     
-            ):null}
-        </li>
-    )
-}
-export default Province
+export default function Provinces(props) {
+  const { index, name, cities } = props;
+  const [clicked, setClicked] = React.useState(false);
+  function handleClicked() {
+    setClicked(!clicked);
+  }
+  return (
+    <li>
+      <h3 id={"state" + (index + 1)} onClick={handleClicked}>
+        {name}
+      </h3>
+      {clicked ? (
+        <ul>
+          {cities.map((item) => (
+            <City
+              name={item.name}
+              towns={item.towns}
+              index={cities.indexOf(item)}
+            />
+          ))}
+        </ul>
+      ) : null}
+    </li>
+  );
